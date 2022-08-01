2a640934422751ba50151202ca5060e388d50b3d
vicky phung <vickyphung@gmail.com>
2022-07-31 23:52:10 -0400

    updated users

--- models/user.js
+++ models/user.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose")
 
 const userSchema = new Schema({
   name: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
   email: { type: String, required: false, unique: false },
-  favorites: [ { type: Schema.Types.ObjectId, ref: 'place'} ],
-  reviews: [ { type: Schema.Types.ObjectId, ref: 'review' } ],
-  posts: [ { type: Schema.Types.ObjectId, ref: 'place' } ]
-})
+  password: { type: String, required: true },
+  favorites: [{ type: Schema.Types.ObjectId, ref: "place" }],
+  reviews: [{ type: Schema.Types.ObjectId, ref: "review" }],
+  posts: [{ type: Schema.Types.ObjectId, ref: "place" }],
+});
 
 const user = mongoose.model('user', userSchema)
 
